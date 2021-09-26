ec92e1c3a57485920f40061d0c40c6d473a9e4ef
voorhs <ilya_alekseev_2016@list.ru>
2021-09-26 15:03:06 +0300

    Ввод строки

--- Task 1. Dynamic String Array/main.c
+++ Task 1. Dynamic String Array/main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char* read_string() {
+    char *result = (char*)malloc(sizeof(char)), c;
+
+    int size_of_str = 0, size_alloc = sizeof(char);
+
+    while ((c = getchar()) != '\n') {
+        if (++size_of_str > size_alloc) {
+            size_alloc *= 2;
+            result = realloc(result, size_alloc);
+        }
+        result[size_of_str - 1] = c;
+    }
+
+    if (size_of_str == size_alloc)
+        result = realloc(result, size_of_str + 1);
+    result[size_of_str] = '\0';
+
+    if (size_of_str = 0)
+        return NULL;
+    else
+        return result;
+}
 
 int main() {
-    printf("%s", "Hello world!");
+    char *s;
+    s = read_string();
+    printf("result is %s\n", s);
+    return 0;
 }
