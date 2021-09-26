e4f18550872f7fb3ed92caafe4d56a8c87fa2fe6
voorhs <ilya_alekseev_2016@list.ru>
2021-09-26 16:57:48 +0300

    Полный функционал, но с утечками памяти

--- Task 1. Dynamic String Array/main.c
+++ Task 1. Dynamic String Array/main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* read_string();                
+char** read_str_array();            
+void print_str_array(char**);
+int array_length(char**);
+void sort_str_array(char**, int);
+
+int main() {
+    char **sarray = read_str_array();
+
+    printf("\n%s\n", "You typed:");
+    print_str_array(sarray);
+
+    sort_str_array(sarray, array_length(sarray));
+    printf("\n%s\n", "Sorted:");
+    print_str_array(sarray);
+    printf("\n\n");
+
+    free(sarray);
+    return 0;
+}
+
 
 char* read_string() {
     char *result = (char*)malloc(sizeof(char)), c;
@@ -7,7 +30,7 @@ char* read_string() {
     int size_of_str = 0, size_alloc = sizeof(char);
 
     while (((c = getchar()) != '\n') && (c != EOF)) {
-        if (++size_of_str > size_alloc) {
+        if (++size_of_str * sizeof(char) > size_alloc) {
             size_alloc *= 2;
             result = realloc(result, size_alloc);
         }
@@ -27,10 +50,10 @@ char* read_string() {
 char** read_str_array() {
     char **result = (char**)malloc(sizeof(char*));
 
-    int size_alloc = 1, str_count = 0;
+    int size_alloc = sizeof(char*), str_count = 0;
 
     while (1) {
-        if (str_count == size_alloc) {
+        if (str_count * sizeof(char*) == size_alloc) {
             size_alloc *= 2;
             result = (char**)realloc(result, size_alloc);
         }            
@@ -55,8 +78,21 @@ void print_str_array(char** sarray) {
     }
 }
 
-int main() {
-    char **sarray = read_str_array();
-    print_str_array(sarray);
-    return 0;
+int array_length(char** arr) {
+    int res;
+    for (res = 0; arr[res]; res++) {}
+    return res;
 }
+
+void sort_str_array(char** sarray, int size)
+{
+    int i, j;
+    for (i = 0; i < size - 1; i++)
+        for (j = size - 1; j > i; j--)
+            if (strcmp(sarray[j - 1], sarray[j]) > 0) {
+                char* temp = sarray[j - 1];
+                sarray[j - 1] = sarray[j];
+                sarray[j] = temp;
+            }
+}
+
