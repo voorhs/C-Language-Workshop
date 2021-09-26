c3c96898152f90c4424a506a78e50a5b643e1717
voorhs <ilya_alekseev_2016@list.ru>
2021-09-26 15:46:52 +0300

    Ввод массива (compiled, but not tested)

--- Task 1. Dynamic String Array/main.c
+++ Task 1. Dynamic String Array/main.c
@@ -6,7 +6,7 @@ char* read_string() {
 
     int size_of_str = 0, size_alloc = sizeof(char);
 
-    while ((c = getchar()) != '\n') {
+    while (((c = getchar()) != '\n') && (c != EOF)) {
         if (++size_of_str > size_alloc) {
             size_alloc *= 2;
             result = realloc(result, size_alloc);
@@ -18,16 +18,39 @@ char* read_string() {
         result = realloc(result, size_of_str + 1);
     result[size_of_str] = '\0';
 
-    if (size_of_str == 0)
+    if (size_of_str == 0) {
+        free(result);
         return NULL;
+    }
     else
         return result;
 }
 
+char** read_str_array() {
+    char **result = (char**)malloc(sizeof(char*));
+
+    int size_alloc = 1, str_count = 0;
+
+    while (1) {
+        if (str_count == size_alloc) {
+            size_alloc *= 2;
+            result = (char**)realloc(result, size_alloc);
+        }            
+        result[str_count] = read_string();
+        if (result[str_count] == NULL)
+            break;
+        else
+            str_count++;                    //check whether str_count can be placed in while or not
+    }
+
+    if (str_count == 0)
+        return NULL;
+    else
+        return result;
+
+}
+
 int main() {
-    char *s;
-    s = read_string();
-    printf("result is %s\n", s);
-    free(s);
+    char **sarray = read_str_array();
     return 0;
 }
