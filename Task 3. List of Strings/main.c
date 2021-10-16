8cc6f83aefb3a419310ef898094807841b50980e
voorhs <ilya_alekseev_2016@list.ru>
2021-10-16 17:47:00 +0300

    init commit

---
+++ Task 3. List of Strings/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list {
+	char* s;
+	struct list* next;
+} List;
+
+List* append(List** L, char* s) {
+    
+}
+
+int main() {
+    return 0;
+}
