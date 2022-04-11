70cff47fb465694211cd6ecfeae579063dc844d0
voorhs <ilya_alekseev_2016@list.ru>
2022-04-11 23:50:14 +0300

    syntactical analisis

--- interpreter/lex.h
+++ interpreter/lex.h
@@ -7,36 +7,36 @@
 
 enum type_of_lex {
     LEX_NULL, /*0*/
-    LEX_AND, LEX_BEGIN, LEX_BOOL, LEX_CONTINUE, LEX_DO, LEX_ELSE, LEX_END, LEX_FALSE, LEX_FOR, LEX_IF, LEX_INT, /*11*/
-    LEX_NOT, LEX_OR, LEX_PROGRAM, LEX_READ, LEX_REAL, LEX_STRING, LEX_THEN, LEX_TRUE, LEX_VAR, LEX_WHILE, LEX_WRITE, /*22*/
-    LEX_FIN, /*23*/
-    LEX_SEMICOLON, LEX_COMMA, LEX_COLON, LEX_ASSIGN, LEX_LPAREN, LEX_RPAREN, LEX_EQ, LEX_LSS, /*31*/
-    LEX_GTR, LEX_PLUS, LEX_PLUSPLUS, LEX_MINUS, LEX_MINUSMINUS, LEX_TIMES, LEX_SLASH, LEX_LEQ, LEX_NEQ, LEX_GEQ, /*41*/
-    LEX_NUM, /*42*/
-    LEX_ID, /*43*/
-    POLIZ_LABEL, /*44*/
-    POLIZ_ADDRESS, /*45*/
-    POLIZ_GO, /*46*/
-    POLIZ_FGO  /*47*/
+    LEX_AND, LEX_CONTINUE, LEX_FALSE, LEX_IF, LEX_INT, /*5*/
+    LEX_NOT, LEX_OR, LEX_PROGRAM, LEX_READ, LEX_BOOL, LEX_STRING, LEX_TRUE, LEX_WHILE, LEX_WRITE, /*14*/
+    LEX_FIN, /*15*/
+    LEX_SEMICOLON, LEX_COMMA, LEX_ASSIGN, LEX_LPAREN, LEX_RPAREN, LEX_LBRACE, LEX_RBRACE, LEX_EQ, LEX_LSS, /*24*/
+    LEX_GTR, LEX_PLUS, LEX_MINUS, LEX_TIMES, LEX_SLASH, LEX_LEQ, LEX_NEQ, LEX_GEQ, /*32*/
+    LEX_INT_CONST, LEX_STRING_CONST, /*34*/
+    LEX_ID, /*35*/
+    POLIZ_LABEL, /*36*/
+    POLIZ_ADDRESS, /*37*/
+    POLIZ_GO, /*38*/
+    POLIZ_FGO  /*39*/
 };
 
 class Lex {
     type_of_lex type;
     int     int_value;
-    float   real_value;
+    bool    bool_value;
     char*   str_value;
 
 public:
 
     Lex(type_of_lex t = LEX_NULL);
     Lex(type_of_lex t, int v);
-    Lex(type_of_lex t, float v);
+    Lex(type_of_lex t, bool v);
     Lex(type_of_lex t, char* buf);
 
     type_of_lex get_type() const;
 
     int     get_int_value() const;
-    float   get_float_value() const;
+    bool    get_bool_value() const;
     char*   get_str_value() const;
 
     friend std::ostream& operator<<(std::ostream& out, Lex l);
